42aec551bc0e6b43505c8a9eb76cbdb480d01043
JE-Chen(陳冠穎) <33644111+JE-Chen@users.noreply.github.com>
2020-07-25 06:40:56 +0800

    Delete Mask_Test.py

--- Mask_Test.py
+++
@@ -1,7 +0,0 @@
-from Mask_Search import Mask_Search
-a=Mask_Search()
-Total='最近的藥局\n'
-for i in (a.Return_Nearby()):
-    Total+=str(i)+'\n'
-print('''----------------------------------------------------------------------------------------''')
-print(Total)
